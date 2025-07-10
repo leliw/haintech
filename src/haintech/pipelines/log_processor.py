e22a820f216dfd152f0e38c2110834adcdeaccdf
Marcin Leliwa <marcin.leliwa@gmail.com>
2025-07-10 10:16:07 +0200

    feat(LogProcessor) Add extra parameter

--- src/haintech/pipelines/log_processor.py
+++ src/haintech/pipelines/log_processor.py
@@ -1,5 +1,6 @@
 import logging
 from pydantic import BaseModel
+from typing import Optional, Callable, Dict
 
 from .base_processor import BaseProcessor
 
@@ -10,8 +11,9 @@ class LogProcessor[I](BaseProcessor[I,I]):
     def __init__(
         self,
         message: str = "{item}",
+        extra: Optional[Callable[[I], Dict]] = None, 
         level: int = logging.INFO,
-        name: str = None,
+        name: Optional[str] = None,
     ):
         """Log the flowing data using the logging module.
 
@@ -25,16 +27,18 @@ class LogProcessor[I](BaseProcessor[I,I]):
         are the keys of the dictionary. Otherwise, the argument is 'item'.
         """
         super().__init__(name=name)
-        self.level = level
         self.message = message
+        self.extra = extra
+        self.level = level
         self._log = logging.getLogger(name or __name__)
 
-    async def process_item(self, data: dict, **kwargs) -> dict:
+    async def process_item(self, data: I, **kwargs) -> I:
         """Log the data."""
+        extra = self.extra(data) if self.extra else None
         if isinstance(data, dict):
-            self._log.log(self.level, self.message.format(**data))
+            self._log.log(self.level, self.message.format(**data), extra=extra)
         elif isinstance(data, BaseModel):
-            self._log.log(self.level, self.message.format(**data.model_dump()))
+            self._log.log(self.level, self.message.format(**data.model_dump()), extra=extra)
         else:
-            self._log.log(self.level, self.message.format(item=data))
+            self._log.log(self.level, self.message.format(item=data), extra=extra)
         return data
