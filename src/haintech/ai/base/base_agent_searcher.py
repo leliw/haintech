c8c22f26c804655f544647beda344cd0ef949112
Marcin Leliwa <marcin.leliwa@gmail.com>
2025-11-16 11:17:58 +0100

    fix(BaseAgentSearcher): agent_search() return None if message isn't provided

--- src/haintech/ai/base/base_agent_searcher.py
+++ src/haintech/ai/base/base_agent_searcher.py
@@ -9,13 +9,14 @@ from haintech.ai.model import AIContext, AIModelInteractionMessage, AIPrompt, RA
 
 class BaseAgentSearcher(BaseRAGSearcher):
     """Agent-based RAG searcher. It uses AI model to generate search query."""
+
     _log = logging.getLogger(__name__)
 
     def __init__(
         self,
         ai_model: BaseAIModel,
         system_instructions: str = "Task: Based on the system description, conversation, and user question, generate one short search query for document retrieval.",
-        prompt: str = "Input:\nSystem: {system_prompt}\nHistory: {conversation_history}\nQuestion: {user_question}\n\nOutput:\nOnly one query between 20 and 100 tokens, no comments or markdown."
+        prompt: str = "Input:\nSystem: {system_prompt}\nHistory: {conversation_history}\nQuestion: {user_question}\n\nOutput:\nOnly one query between 20 and 100 tokens, no comments or markdown.",
     ):
         super().__init__()
         self.ai_task_executor = AITaskExecutor(
@@ -30,7 +31,7 @@ class BaseAgentSearcher(BaseRAGSearcher):
     ) -> AIContext | None:
         if not system_prompt and not history:
             return super().agent_search_sync(system_prompt, history, message)
-        else:
+        elif message and message.content:
             conversation_history = [f"{m.role}: {m.content}" for m in history]
             ret = self.ai_task_executor.execute(
                 system_prompt=system_prompt,
@@ -38,6 +39,8 @@ class BaseAgentSearcher(BaseRAGSearcher):
                 user_question=message.content,
             )
             return AIContext(documents=list(self.search_sync(RAGQuery(text=ret))))  # type: ignore
+        else:
+            return None
 
     async def agent_search_async(
         self,
@@ -47,12 +50,13 @@ class BaseAgentSearcher(BaseRAGSearcher):
     ) -> AIContext | None:
         if not system_prompt and not history:
             return await super().agent_search_async(system_prompt, history, message)
-        else:
+        elif message and message.content:
             conversation_history = [f"{m.role}: {m.content}" for m in history]
             ret = await self.ai_task_executor.execute_async(
                 system_prompt=system_prompt,
                 conversation_history="\n".join(conversation_history),
                 user_question=message.content,
             )
-            return AIContext(documents=[r async for r in self.search_async(RAGQuery(text=ret))]) # type: ignore
-
+            return AIContext(documents=[r async for r in self.search_async(RAGQuery(text=ret))])  # type: ignore
+        else:
+            return None
