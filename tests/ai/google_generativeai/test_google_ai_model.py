551843f3f6bfc13d3ade5b73d10d29bb77a2eaae
Marcin Leliwa <marcin.leliwa@gmail.com>
2025-11-21 14:40:00 +0100

    test: Correct paths

--- tests/ai/google_generativeai/test_google_ai_model.py
+++ tests/ai/google_generativeai/test_google_ai_model.py
@@ -1,5 +1,3 @@
-import pytest
-
 from haintech.ai.google_generativeai.google_ai_model import GoogleAIModel
 
 
@@ -10,7 +8,3 @@ def test_list_models():
     ret = ai_model.get_model_names()
     # Then: At least one gemini model name is returned
     assert any("gemini" in n for n in ret)
-
-
-if __name__ == "__main__":
-    pytest.main([__file__])
